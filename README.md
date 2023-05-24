98b5b85eac7ce2a84b7e2cb9aef8f6cd3fd68bad
Naoki Ikeguchi <n_ikeguchi@yumemi.co.jp>
2023-05-24 23:44:32 +0900

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Action Release Action
+
+> **Warning**  
+> This is not an official product of YUMEMI Inc.
+
+Release TypeScript action with prebuilt bundle.
+
+
+## Getting Started
+
+See [.github/workflows/release.yaml](.github/workflows/release.yaml) for example workflow.
+
+```yaml
+- uses: yumemi-inc/action-release-action@v1
+  with:
+    version: '${{ inputs.version }}'
+```
---
+++ index.ts
@@ -0,0 +1,115 @@
+import { cp } from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { cwd, exit } from 'node:process';
+
+import {
+	error,
+	getInput,
+	getMultilineInput,
+	group,
+	setFailed,
+} from '@actions/core';
+import { exec } from '@actions/exec';
+import { context } from '@actions/github';
+import { Octokit } from '@octokit/rest';
+import { parse } from 'url';
+
+const getInputRequired = (name: string) =>
+	getInput(name, {
+		required: true,
+	});
+
+(async () => {
+	const version = getInputRequired('version');
+	const releaseBranch = getInputRequired('release-branch');
+	const buildCommand = getMultilineInput('build-command', { required: true });
+	const directory = getInputRequired('directory');
+	const token = getInputRequired('token');
+
+	const versionInfo = /^v(?<major>\d+).(?<minor>\d+).(?<patch>\d+).*$/.exec(
+		version,
+	)?.groups as {
+		major: string;
+		minor: string;
+		patch: string;
+	} | null;
+
+	if (versionInfo === null) {
+		setFailed('Unsupported version format. Use `vx.y.z` style.');
+		exit(1);
+	}
+
+	const octokit = new Octokit({
+		baseUrl: context.apiUrl,
+		auth: token,
+	});
+
+	const repository = (await octokit.repos.get({ ...context.repo })).data;
+	const runInDist = { cwd: resolve(cwd(), directory) };
+
+	await group('Initialising release branch', async () => {
+		await exec('git', [
+			'config',
+			'--global',
+			'user.name',
+			'github-actions[bot]',
+		]);
+
+		await exec('git', [
+			'config',
+			'--global',
+			'user.email',
+			'github-actions[bot]@users.noreply.github.com',
+		]);
+
+		const gitUrl = new URL(repository.clone_url);
+		gitUrl.username = 'oauth2';
+		gitUrl.password = token;
+
+		await exec('git', [
+			'config',
+			'--global',
+			`url.${gitUrl.toString()}.insteadOf`,
+			repository.clone_url,
+		]);
+
+		await exec('git', ['init', '-b', releaseBranch], runInDist);
+		await exec(
+			'git',
+			['remote', 'add', 'origin', repository.clone_url],
+			runInDist,
+		);
+
+		await exec('git', ['pull', '--set-upstream', 'origin']);
+	});
+
+	await group('Running build commands', async () => {
+		await Promise.all(
+			buildCommand.map(async (command) => await exec('sh', ['-c', command])),
+		);
+
+		await cp('action.yml', resolve(directory, 'action.yml'));
+	});
+
+	await group('Committing changes', async () => {
+		await exec('git', ['add', '.'], runInDist);
+		await exec('git', ['commit', '-m', `feat: Release ${version}`], runInDist);
+		await exec('git', ['tag', `v${versionInfo.major}`], runInDist);
+		await exec('git', ['push', '--tags', 'origin', releaseBranch], runInDist);
+	});
+
+	await group('Creating a release', async () => {
+		await octokit.repos.createRelease({
+			...context.repo,
+			name: version,
+			tag_name: version,
+			target_commitish: releaseBranch,
+			generate_release_notes: true,
+		});
+	});
+})()
+	.then()
+	.catch((e) => {
+		error(e);
+		exit(1);
+	});
